95e6685546c1ecb4fedf2817f7890f7f870d2f93
Manoj <Manoj@Manoj-PC>
2022-02-09 23:07:31 +0530

    create eureka server as registry microservice

---
+++ RegistryMicroservice/src/main/java/com/example/microservices/rms/RegistryMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.microservices.rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryMicroserviceApplication.class, args);
+	}
+
+}
