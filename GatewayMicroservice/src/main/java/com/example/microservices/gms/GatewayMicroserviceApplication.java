11281143f2a9dd4dcd71e1ec69f881011a5e118c
Manoj <Manoj@Manoj-PC>
2022-02-10 20:22:36 +0530

    Updated hystrix to handle failover scenario

--- GatewayMicroservice/src/main/java/com/example/microservices/gms/GatewayMicroserviceApplication.java
+++ GatewayMicroservice/src/main/java/com/example/microservices/gms/GatewayMicroserviceApplication.java
@@ -3,11 +3,11 @@ package com.example.microservices.gms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableHystrix
+@EnableHystrix
 public class GatewayMicroserviceApplication {
 
 	public static void main(String[] args) {
